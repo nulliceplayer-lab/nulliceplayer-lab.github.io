490ebf18134b4ebaaa57a9fc27021bedae05749d
Ice Null <nulliceplayer@gmail.com>
2026-05-28 18:17:14 +0900

    Update slow_script.js

--- tools/slow_script.js
+++ tools/slow_script.js
@@ -33,21 +33,7 @@ const newWindow = window.open('about:blank', '_blank');
 if (newWindow) {
     // 2. 挿入したいHTMLコードを定義する
     const htmlContent = '
-        <!DOCTYPE html>
-        <html lang="ja">
-        <head>
-            <meta charset="UTF-8">
-            <title>挿入されたページ</title>
-            <style>
-                body { font-family: sans-serif; text-align: center; padding-top: 50px; background-color: #f0f0f0; }
-                h1 { color: #333; }
-            </style>
-        </head>
-        <body>
-            <h1>Hello, about:blank!</h1>
-            <p>このコンテンツはJavaScriptによって動的に挿入されました。</p>
-        </body>
-        </html>
+        <h1>Hello About:blank!!!</h1>
     ';
 
     // 3. ドキュメントを開いてHTMLを書き込み、閉じる
