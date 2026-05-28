c4c8cd1cc25722ecb128c87a064b62cc41e89a0a
Ice Null <nulliceplayer@gmail.com>
2026-05-28 21:02:27 +0900

    もう手段が思いつかない

--- tools/slow_script.js
+++ tools/slow_script.js
@@ -19,41 +19,8 @@
                         <h1>Slow_scriptへようこそ！</h1>
                         <p>Slow_scriptはabout:blankを経由して<b>履歴に残さない</b>ツールサイトです。</p>
                         <p>さっそくですがYoutube見たいですよね？そんなあなたには<b>Youtubeダウンローダー</b>を使いましょう。YoutubeダウンローダーはYoutubeのリンクを打つだけでなんとYoutubeの動画が見れます！ですが、ブロックツールがダウンロードを妨害する可能性があります。</p>
-<button oneclick="Youtube-downloader()">Youtubeダウンローダーへ</button>
-<script>
-function Youtube-downloader() {
+<button>Youtubeダウンローダーへ</button>
 
-const newWindow = window.open('about:blank', '_blank');
-
-if (newWindow) {
-    const htmlContent = `
-        <!DOCTYPE html>
-        <html lang="ja">
-        <head>
-            <meta charset="UTF-8">
-            <title>挿入されたページ</title>
-            <style>
-                body { font-family: sans-serif; text-align: center; padding-top: 50px; background-color: #f0f0f0; }
-                h1 { color: #333; }
-            </style>
-        </head>
-        <body>
-            <h1>Hello, about:blank!</h1>
-            <p>このコンテンツはJavaScriptによって動的に挿入されました。</p>
-        </body>
-        </html>
-    `;
-
-    newWindow.document.open();
-    newWindow.document.write(htmlContent);
-    newWindow.document.close();
-} else {
-    alert('ポップアップがブロックされました。ブラウザの設定を許可してください。');
-}
-
-
-}
-</script>
     
     `;
             
